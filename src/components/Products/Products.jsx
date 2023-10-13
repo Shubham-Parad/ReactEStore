a789d860d7269f010c927ed4560cb67ccf4010d9
Shubham-Parad <shubhamparad19@gmail.com>
2023-10-13 12:53:55 +0530

    React Estore

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,26 @@
+import "./Products.scss";
+import Product from './Product/Product';
+
+
+const Products = ({ products, innerPage, headingText }) => {
+    return (
+        <div className="products-container">
+            {!innerPage && <div className="sec-heading">{headingText}</div>}
+            <div className="products">
+                {products?.data?.map((item) => (
+                    <Product
+                        key={item.id}
+                        id={item.id}
+                        data={item.attributes}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+
+};
+
+export default Products;
+
+
+// The optional chaining (?.) operator accesses an object's property or calls a function. If the object accessed or function called using this operator is undefined or null, the expression short circuits and evaluates to undefined instead of throwing an error.
